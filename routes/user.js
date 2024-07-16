6e3c8472fa5db2ef4c328bb3c9c479bf03d2aa04
muendo <horracemuendo34.com>
2024-07-17 00:38:55 +0300

    done with the routes

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const db =require('../db/dbConfig')
 
+router.post("/signup", (req, res) => {
+    const sql = "INSERT INTO users(`firstname`,`lastname`,`email`,`password` Values (?))"
+    const Values = [
+        req.body.firstname,
+        req.body.lastname,
+        req.body.email,
+        req.body.password,
+    ]
+    db.query(sql, [Values], (err, data) => {
+        if (err) {
+            return res.json(err)
+        } else {
+            res.json(data)
+        }
+    })
+})
