6f81b9fc472050d40613ca7419e03eeb9ca695b1
Horrace Muendo <horracemuendo34@gmail.com>
2024-10-09 22:18:26 +0300

    testing the enquiries endpoints

--- routes/Enquiries.js
+++ routes/Enquiries.js
@@ -2,4 +2,15 @@ const express = require('express');
 const router = express.Router();
 const db = express('../db/dbConfigure');
 
+router.get('/enquirires', (res,req)=>{
+    const sql = 'SELECT * from enquirires'
 
+    db.query(sql, (err,result)=>{
+        if (err) {
+            console.log('Database error:', err)
+            return res.status(500).json({error: 'Internal server error plaease try again later  '})
+        }
+        res.json(result.rows)
+    })
+
+})
