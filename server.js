9786a5b808034a10ec6ecd81403a072c6242ac18
Horrace Muendo <horracemuendo34@gmail.com>
2024-10-11 14:55:15 +0300

    configured the routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const userRouter = require('./routes/user')
 const productsRouter = require ('./routes/product')
+const enquiriesRouter = require ('./routes/Enquiries')
+const customerRouter = require ('./routes/customer')
+
 const app = express();
 const cors = require('cors');
 const port = 6969;
@@ -13,6 +16,10 @@ app.use(express.json())
 //calling the routes
 app.use('/users', userRouter)
 app.use('/products',productsRouter)
+app.use('/customers', customerRouter)
+app.use('/enquiries',enquiriesRouter)
+
+
 
 
 app.listen(port, () => {
